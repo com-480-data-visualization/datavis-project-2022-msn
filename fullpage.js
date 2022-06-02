71e41fa6d0c14babcb91477be6138dc267f9bc36
Nicolas Raulin <nicolas.raulin@epfl.ch>
2022-06-02 14:34:29 +0200

    Replaced old theme for FullPageJs

---
+++ fullpage.js
@@ -0,0 +1,9 @@
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    // scrollHorizontally: true,
+    navigation: true,
+    navigationTooltips: ['firstSlide', 'secondSlide', 'thirdSlide'],
+    showActiveTooltip: false,
+    controlArrows: false,
+    slidesNavigation: true
+})
